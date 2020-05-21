24145028eb163b41024a4e58420db37e4e010cf3
Han <han13066022700@gmail.com>
2020-05-21 14:10:36 +1000

    debug

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({'hello': 'there'});
+    res.send({'hello': 'Han'});
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT);
+const server = app.listen(process.env.PORT || 8080, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Example app listening at http://${host}:${port}');
+});
